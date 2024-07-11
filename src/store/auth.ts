f6c88e61370e6055eb2286d3aa32f665fc5b8f5a
Kolawole <kolakachi@gmail.com>
2024-07-11 01:44:44 +0100

    set auth store for user management

---
+++ src/store/auth.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia';
+import { setToken, getToken, removeToken } from '@/utils/auth';
+
+interface User {
+  uuid: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  avatar: string | null;
+  address: string;
+  phone_number: string;
+  is_marketing: number;
+  updated_at: string;
+  created_at: string;
+  token: string;
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    token: getToken(),
+    user: null as User | null,
+  }),
+  actions: {
+    setUser(data: User) {
+      this.user = data;
+    },
+    setToken(token: string) {
+        this.token = token;
+        setToken(token);
+    },
+    logout() {
+      this.user = null;
+      this.token = null;
+      removeToken();
+    },
+    isLoggedIn() {
+      return !!this.token;
+    }
+  }
+});
