640b0aab1e146acb1b32314b4500bc40b0c57db9
Kolawole <kolakachi@gmail.com>
2024-07-11 03:12:50 +0100

    update axios interceptor

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,18 +1,21 @@
-import axios from 'axios';
-import { getToken } from '@/utils/auth';
+import axios from "axios";
+import { getToken } from "@/utils/auth";
 
 const instance = axios.create({
-  baseURL: 'https://pet-shop.buckhill.com.hr/api/v1',
+  baseURL: "https://pet-shop.buckhill.com.hr/api/v1",
 });
 
-instance.interceptors.request.use(config => {
-  const token = getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+instance.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+);
 
 export default instance;
