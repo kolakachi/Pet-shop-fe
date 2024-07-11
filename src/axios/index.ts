d6f2cb5582879d26e888bf8c9cfd25ddcff13c9d
Kolawole <kolakachi@gmail.com>
2024-07-11 01:47:32 +0100

    create axios interceptor

---
+++ src/axios/index.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { getToken } from '@/utils/auth';
+
+const instance = axios.create({
+  baseURL: 'https://pet-shop.buckhill.com.hr/api/v1',
+});
+
+instance.interceptors.request.use(config => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+export default instance;
